30acd45d2d9a4ca87b15efabaee3f503e8ab50f7
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-14 19:44:22 -0300

    saida do fetchProducts alterado

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,7 +2,7 @@ const fetchProducts = async (query) => {
   const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
   const dataObj = await fetch(API_URL)
     .then((response) => response.json())
-    .then((data) => data.results)
+    .then((data) => data)
     .catch((error) => `Algo deu errado :( \n${error}`);
   return dataObj;
 };
--- script.js
+++ script.js
@@ -43,8 +43,8 @@ function createCartItemElement({ sku, name, salePrice }) {
 }
 
 async function init() {
-  const products = await fetchProducts('computador');
-  products.forEach((element) => {
+  const { results } = await fetchProducts('computador');
+  results.forEach((element) => {
     const name = element.title;
     const sku = element.id;
     const image = element.thumbnail;
