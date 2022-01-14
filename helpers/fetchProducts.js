b360b051062437095df8525171a300c51e0247cc
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-14 19:07:14 -0300

    funcao fetchProducts criada

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async () => {
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const dataObj = await fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => data.results)
+    .catch((error) => `Algo deu errado :( \n${error}`);
+  return dataObj;
 };
 
 if (typeof module !== 'undefined') {
