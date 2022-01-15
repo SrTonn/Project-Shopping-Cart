506519c670ea2b6d6ad7f2d7e1fab13a9f0f14d9
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-14 21:30:16 -0300

    função fetchItem criada

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (query) => {
+  const API_URL = `https://api.mercadolibre.com/items/${query}`;
+  const dataObj = await fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => error);
+  return dataObj;
 };
 
 if (typeof module !== 'undefined') {
